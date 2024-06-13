ed159e3fdcd7e72feac6b608cb1c374fda9a9d4e
Bailey Thorp <147442869+BaileyThorpTecht@users.noreply.github.com>
2024-06-14 09:18:51 +1200

    made scatter plot just for the custom model. the custom model works but is not good

--- creating_model.py
+++ creating_model.py
@@ -13,54 +13,57 @@ from joblib import load
 import tensorflow as tf
 import keras
 
-def TrainTestSplit(input, output, testSize):
-    
-    #rng = np.random.default_rng()
-    #rand= rng.integers(100)
-    rand = 37
-    Xtest = input.sample(frac=testSize, random_state=rand)
-    Xtrain = input.drop(Xtest.index)
-    
-    Ytest = output.sample(frac=testSize, random_state=rand)
-    Ytrain = output.drop(Ytest.index)
-    
-    return Xtrain, Xtest, Ytrain, Ytest
-    
-
-########## CUSTOM MODEL CODE (this function)##########
+#make the model work well
+#remember to upload on the other repo
 def RunModel (model, Xtrain, Xtest, Ytrain, Ytest, sc):
-    
+    #train and test the model
     model.fit(Xtrain, Ytrain)    
     predictions = model.predict(Xtest)
     
-    scaleUpOutput = False
-    if (scaleUpOutput):
-        #(doesnt work)
-        Ytest = sc.inverse_transform(Ytest)
-        predictions = sc.inverse_transform(predictions)[0]
+    #turn predictions and Ytest back into normal values
+    try:
+        inv_predictions = scOutput.inverse_transform(predictions)
+    except:
+        inv_predictions = scOutput.inverse_transform([predictions])[0]
+
+    inv_Ytest = scOutput.inverse_transform(Ytest)
+    
+    #make scatter chart for the custom model
+    if type(model).__name__ == "Sequential":    
+        plt.scatter(inv_Ytest, inv_predictions)
+        plt.xlabel("Actual")
+        plt.ylabel("Prediction")
+        plt.title(type(model).__name__)
+        ax = plt.gca()
+        ax.set_xlim([0, 100000])
+        ax.set_ylim([0, 100000])
+        plt.show()
+    
+    #get rmse from model's performance
+    scaleRMSE = True 
+    if scaleRMSE:
+        rmse = mean_squared_error(inv_Ytest, inv_predictions, squared=False)
+    else:
+        rmse = mean_squared_error(Ytest, predictions, squared=False)
         
-    rmse = mean_squared_error(Ytest, predictions, squared=False)
     return rmse
 
 
 def CustomModel(x_train):
-    try:
-        customModel = load("custom_model.joblib")
-        return customModel
-    except:
-        #creating the model and adding layers to it
-        model = keras.models.Sequential()
-        model.add(keras.layers.Dense(units=32, activation='relu', input_dim=len(x_train.columns)))
-        model.add(keras.layers.Dense(units=64, activation='relu'))
-        model.add(keras.layers.Dense(units=64, activation='relu'))
-        model.add(keras.layers.Dense(units=64, activation='relu'))
-        model.add(keras.layers.Dense(units=64, activation='relu'))
-        model.add(keras.layers.Dense(units=1, activation='sigmoid'))
+    #creating the model and adding layers to it
+    model = keras.models.Sequential()
+    model.add(keras.layers.Dense(units=320, activation='relu', input_dim=len(x_train.columns)))
+    #model.add(keras.layers.Dropout(0.2))
+    model.add(keras.layers.Dense(units=640, activation='relu'))
+    #model.add(keras.layers.Dropout(0.2))
+    model.add(keras.layers.Dense(units=640, activation='relu'))
+    #model.add(keras.layers.Dropout(0.2))
+    model.add(keras.layers.Dense(units=1, activation='sigmoid'))
 
-        model.compile(loss="binary_crossentropy", optimizer="sgd")    
-        
-        dump(model, "custom_model.joblib")
-        return model
+    model.compile(loss="binary_crossentropy", optimizer="adam")    
+    
+    dump(model, "custom_model.joblib")
+    return model
 
 
 
@@ -68,7 +71,7 @@ def CustomModel(x_train):
 #import dataset
 dataset = pd.read_csv("Car_Purchasing_Data.csv")
  
-#creating input data`set by removing irrelevant columns
+#creating input data set by removing irrelevant columns
 #### input => X ####
 inputData = dataset.filter(["Age", "Annual Salary", "Credit Card Debt", "Net Worth"])
 
@@ -99,7 +102,6 @@ modelList = []
 rmseList = []
 modelNameList = []
 
-########## CUSTOM MODEL CODE (1 line) ##########
 modelList.append(CustomModel(inputTrain))
 modelList.append(linear_model.LinearRegression())
 modelList.append(linear_model.Ridge())
@@ -118,8 +120,7 @@ for model in modelList:
     rmseList.append(rmse)
     
     modelName = type(model).__name__
-########## CUSTOM MODEL CODE (2 lines) ##########
-    if modelName is "Sequential":
+    if modelName == "Sequential":
         modelName = "Custom Model"
     modelNameList.append(modelName)
         
@@ -132,5 +133,6 @@ plt.title("RMSE (Root Mean Squared Error) of Regression Models")
 plt.xticks(rotation=25, ha="right")
 plt.subplots_adjust(bottom=0.20)
 plt.show()
+
 
 
